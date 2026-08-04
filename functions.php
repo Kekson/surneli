66d387dfb934f3961818d490933afb2e844f1961
Bacho Kekelidze <bacho.kekelidze01@geolab.edu.ge>
2026-08-05 00:45:40 +0400

    css cache

--- functions.php
+++ functions.php
@@ -8,7 +8,12 @@ function porto_child_css()
 {
     // porto child theme styles
     wp_deregister_style('styles-child');
-    wp_register_style('styles-child', esc_url(get_stylesheet_directory_uri()) . '/style.css');
+    wp_register_style(
+    'styles-child',
+    esc_url(get_stylesheet_directory_uri()) . '/style.css',
+    array(),
+    filemtime(get_stylesheet_directory() . '/style.css')
+);
     wp_enqueue_style('styles-child');
 
     if (is_rtl()) {
