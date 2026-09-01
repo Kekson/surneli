3f671bae30e8e353b434044cef1a11ddb48a3299
Bacho Kekelidze <bacho.kekelidze01@geolab.edu.ge>
2026-09-01 23:42:06 +0400

    hide user enumaration{

--- functions.php
+++ functions.php
@@ -1344,3 +1344,9 @@ function surneli_add_gender_gradient_class( $classes, $product ) {
 
 	return $classes;
 }
+
+add_filter('rest_endpoints', function($endpoints) {
+    if (isset($endpoints['/wp/v2/users'])) unset($endpoints['/wp/v2/users']);
+    if (isset($endpoints['/wp/v2/users/(?P<id>[\d]+)'])) unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+    return $endpoints;
+});
